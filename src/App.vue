04741c182c8ef88daef9685a528beca8ba1d5cb7
aqibtareen <38519950+aqibtareen@users.noreply.github.com>
2023-02-15 00:34:32 +0530

    v2
    v2

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1>Aqib Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Click to play</button>
+  <BlockItem v-if="isPlaying" :delay="delay" @end="endGame" />
+  <ResultItem v-if="showResults" :score="score" />
+</template>
+
+<script>
+import BlockItem from './components/BlockItem.vue'
+import ResultItem from './components/ResultItem.vue'
+export default {
+  name: 'App',
+  components: { BlockItem, ResultItem },
+  data () {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start () {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    },
+    endGame (reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/ResultItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <p>Reaction Time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data () {
+    return {
+      rank: null
+    }
+  },
+  mounted () {
+    if (this.score < 250) {
+      this.rank = 'Very Fast speed'
+    } else if (this.score < 400) {
+      this.rank = 'Average Fast speed'
+    } else {
+      this.rank = 'slow speed......'
+    }
+  }
+}
+</script>
+
+<style>
+.rank {
+  color: rgb(8, 27, 21);
+  font-weight: bold;
+}
+</style>
