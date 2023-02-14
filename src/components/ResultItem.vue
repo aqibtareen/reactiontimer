c883af7525d34218f59feb5e0c736148e7718d68
aqibtareen <38519950+aqibtareen@users.noreply.github.com>
2023-02-15 01:24:10 +0530

    v4
    v4

--- src/components/ResultItem.vue
+++ src/components/ResultItem.vue
@@ -1,6 +1,7 @@
 <template>
     <p>Reaction Time - {{ score }} ms</p>
-    <p class="rank">{{ rank }} Kindly click on reload before playing to get accurate result</p>
+    <p class="rank">{{ rank }} </p>
+    <p class="rank">Kindly click on reload to get accurate results</p>
 </template>
 
 <script>
@@ -13,13 +14,13 @@ export default {
   },
   mounted () {
     if (this.score < 400) {
-      this.rank = 'Very Fast speed'
+      this.rank = 'Very Fast Speed'
     } else if (this.score < 600) {
-      this.rank = 'Average Fast speed'
+      this.rank = 'Average Fast Speed'
     } else if (this.score < 1000) {
-      this.rank = 'slow speed......'
+      this.rank = 'Slow Speed......'
     } else {
-      this.rank = 'out of time'
+      this.rank = 'Out Of Time'
     }
   }
 }
@@ -27,7 +28,9 @@ export default {
 
 <style>
 .rank {
-  color: rgb(8, 27, 21);
+  font-size: 1.4em;
+  color: green;
   font-weight: bold;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
 </style>
